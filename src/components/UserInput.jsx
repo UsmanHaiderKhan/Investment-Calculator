f7543a19d15c03f836c6fe69f90549d7e658e118
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2024-10-07 18:50:37 +0200

    ♻️ Update : add two way binding in UserInput component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
 export default function UserInput() {
-    return (<section id="user-input">
-        <div className="input-group">
-            <p>
-                <label>  Initial Investment  </label>
-                <input type="number" required id="initial-investment" placeholder="Initial Investment" />
-            </p>
-            <p>
-                <label>  Annual Investment  </label>
-                <input type="number" required id="Annual-investment" placeholder="Annual Investment" />
-            </p>
-            <p>
-                <label>  Duration  </label>
-                <input type="number" required id="Duration" placeholder="Duration" />
-            </p>
-        </div>
-    </section>
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1000,
+        expectedReturn: 8,
+        duration: 10
+    });
+    // Function
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            }
+        });
+    }
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>  Initial Investment  </label>
+                    <input type="number" required id="initial-investment" placeholder="Initial Investment" value={userInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)} />
+                </p>
+                <p>
+                    <label>  Annual Investment  </label>
+                    <input type="number" required id="Annual-investment" placeholder="Annual Investment" value={userInput.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)} />
+                </p>
+                <p>
+                    <label>  Expected Return  </label>
+                    <input type="number" required id="expected-return" placeholder="Expected Return" value={userInput.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)} />
+                </p>
+                <p>
+                    <label>  Duration  </label>
+                    <input type="number" required id="Duration" placeholder="Duration" value={userInput.duration} onChange={(event) => handleChange('duration', event.target.value)} />
+                </p>
+            </div>
+        </section>
     );
 }
